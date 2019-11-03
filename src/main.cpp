546a41b86b26f88e85ae9190d0936eb5e42022fc
Andrey Yaromenok/PC <ayaromenok@gmail.com>
2019-11-03 19:44:32 +0100

    #1 app stub added

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
+#include <GLES3/gl31.h>
+#include <fcntl.h>
+#include <gbm.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]) 
+{
+	printf ("GLES31Compute for RPi4\n");
+	return 0;
+}
